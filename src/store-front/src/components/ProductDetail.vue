c07c3a86b716d5c8d23cab79684c5c472cc433b3
sunil-pinto_prov <sunil.pinto@providence.org>
2025-03-04 13:42:40 -0800

    pd

--- src/store-front/src/components/ProductDetail.vue
+++ src/store-front/src/components/ProductDetail.vue
@@ -1,25 +1,27 @@
 <template>
-  <br>
-  <div class="product-detail" v-if="productExists">
-    <div class="product-image">
-      <img :src="product.image" :alt="product.name" />
-    </div>
-    <div class="product-info">
-      <h2>{{ product.name }}</h2>
-      <small>Product ID: {{ product.id }}</small>
-      <p>{{ product.description }}</p>
-      <div class="product-controls">
-        <p>
-          <b>Price: <span class="price">{{ product.price }}</span></b>
-        </p>
-        <input type="number" v-model="quantity" min="1" class="quantity-input" />
-        <button @click="addToCart">Add to Cart</button>
+  <div>
+    <br>
+    <div class="product-detail" v-if="productExists">
+      <div class="product-image">
+        <img :src="product.image" :alt="product.name" />
+      </div>
+      <div class="product-info">
+        <h2>{{ product.name }}</h2>
+        <small>Product ID: {{ product.id }}</small>
+        <p>{{ product.description }}</p>
+        <div class="product-controls">
+          <p>
+            <b>Price: <span class="price">{{ product.price }}</span></b>
+          </p>
+          <input type="number" v-model="quantity" min="1" class="quantity-input" />
+          <button @click="addToCart">Add to Cart</button>
+        </div>
       </div>
     </div>
-  </div>
-  <div class="product-detail" v-else>
-    <img src="../assets/404.jpg" alt="Product not found" />
-    <h3>Opps! That product was not found...</h3>
+    <div class="product-detail" v-else>
+      <img src="../assets/404.jpg" alt="Product not found" />
+      <h3>Opps! That product was not found...</h3>
+    </div>
   </div>
 </template>
 
