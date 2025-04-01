f8ffeea67c39fcb7fa0b73d4b1ada9143b259399
sunil-pinto_prov <sunil.pinto@providence.org>
2025-03-31 17:28:54 -0700

    debug2

--- src/ai-service/routers/description_generator.py
+++ src/ai-service/routers/description_generator.py
@@ -14,24 +14,25 @@ geminiai_api_key = os.environ.get("GEMINI_API_KEY")
 use_geminiai = os.environ.get("USE_GEMINIAI")
 print("<< "+ use_geminiai + " >> ")
 print(" << "+ geminiai_api_key + " >> ")
-
+print("<< CHKPOINT -2 >> ")
 if isinstance(use_geminiai, str) == False or (use_geminiai.lower() != "true" and use_geminiai.lower() != "false"):
     raise Exception("USE_GEMINIAI environment variable must be set to 'True' or 'False' string not boolean")
 
 if (isinstance(geminiai_api_key, str) == False or geminiai_api_key == ""):
     raise Exception("GEMINI_API_KEY environment variable must be set")
-
+print("<< CHKPOINT -1 >> ")
 
 genai.configure(api_key=geminiai_api_key)
 
 # Define the description API router
 description: APIRouter = APIRouter(prefix="/generate", tags=["generate"])
-
+print("<< CHKPOINT 0 >> ")
 # Define the Product class
 class Product:
     def __init__(self, product: Dict[str, List]) -> None:
         self.name: str = product["name"]
         self.tags: List[str] = product["tags"]
+        print("<< CHKPOINT 100 >> ")
         self.image: str = product["image"]
  
 # Define the post_description endpoint
